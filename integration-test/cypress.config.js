b302d5b8baf8d8fe6c0875659558b52c49671f98
Lucas <lucasccitolino19@gmail.com>
2025-05-13 17:08:53 -0300

    consertando integration test parte 18

--- integration-test/cypress.config.js
+++ integration-test/cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("cypress");
+const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5500',
-    supportFile: 'cypress/support/e2e.js',
-    setupNodeEvents(on, config) {
-    }
+    supportFile: path.resolve(__dirname, 'cypress/support/e2e.js'),
+    setupNodeEvents(on, config) {}
   }
 });
